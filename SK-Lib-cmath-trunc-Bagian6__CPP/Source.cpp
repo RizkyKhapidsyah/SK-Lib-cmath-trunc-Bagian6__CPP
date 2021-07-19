1f01e0b499c32e64eb98b811363b2fd31c53c800
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-07-19 09:39:01 +0700

    Add project files.

---
+++ SK-Lib-cmath-trunc-Bagian6__CPP/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+    int x = 782;
+    double hasil;
+    hasil = trunc(x);
+    cout << "trunc(" << x << ") = " << hasil << endl;
+
+    _getch();
+    return 0;
+}
